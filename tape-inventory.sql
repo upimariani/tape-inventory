938fb093e1bf5488783d7e78d1d78ca85f52fd8f
upimariani <upimariani.uniku17@gmail.com>
2024-01-28 07:53:55 +0700

    add revisi

--- tape-inventory.sql
+++ tape-inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 29, 2023 at 01:29 AM
+-- Generation Time: Jan 07, 2024 at 03:17 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -45,9 +45,9 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id_barang`, `id_kategori`, `id_user`, `nama_barang`, `deskripsi`, `keterangan`, `harga`, `stok`, `gambar`) VALUES
-(1, 2, 4, 'Peyeum 100 biji', 'Peyeum ember isi 100 biji', 'pcs', '10000', 4, '219983.png'),
-(2, 2, 4, 'Peyeum 10 biji', 'peyeuem mika isi 10', 'pcs', '6000', 95, '42059061.png'),
-(3, 3, 4, 'Roundup', 'Kerupuk seblak original', 'kg', '8880', 1000, '2199831.png');
+(1, 2, 4, 'Ketan', 'Ketan Type 45', 'kg', '10000', 3, 'images.jpeg'),
+(2, 2, 4, 'Ragi', 'ragi peyeum ', 'kg', '6000', 91, '5f40ebb936c96.jpg'),
+(3, 3, 4, 'Perisa Pandan', 'pewarna hijau', 'pcs', '5000', 1000, 'images_(1).jpeg');
 
 -- --------------------------------------------------------
 
@@ -68,7 +68,10 @@ CREATE TABLE `barang_keluar` (
 
 INSERT INTO `barang_keluar` (`id_bar_keluar`, `id_bar_masuk`, `tgl_keluar`, `stok_keluar`) VALUES
 (1, 1, '2023-12-29', 1),
-(2, 2, '2023-12-29', 5);
+(2, 2, '2023-12-29', 5),
+(3, 1, '2023-12-30', 1),
+(4, 2, '2024-01-07', 2),
+(5, 8, '2024-01-07', 2);
 
 -- --------------------------------------------------------
 
@@ -89,8 +92,14 @@ CREATE TABLE `barang_masuk` (
 --
 
 INSERT INTO `barang_masuk` (`id_bar_masuk`, `id_barang`, `id_pengajuan`, `stok_masuk`, `sisa_stok`) VALUES
-(1, 1, 1, 2, 1),
-(2, 2, 1, 10, 5);
+(1, 1, 1, 2, 0),
+(2, 2, 1, 10, 3),
+(3, 1, 2, 2, 2),
+(4, 2, 2, 10, 10),
+(5, 1, 3, 2, 2),
+(6, 2, 3, 9, 9),
+(7, 1, 4, 2, 2),
+(8, 2, 4, 10, 8);
 
 -- --------------------------------------------------------
 
@@ -132,7 +141,10 @@ CREATE TABLE `pengajuan` (
 --
 
 INSERT INTO `pengajuan` (`id_pengajuan`, `id_user`, `tgl_pengajuan`, `total_pengajuan`, `status_pengajuan`, `bukti_pembayaran`) VALUES
-(1, 4, '2023-12-29', '80000', 3, 'Ini-Dia-Bukti-Transfer-Mandiri-Dari-ATM-mBanking-dan-Internet-Banking-Mandiri-1.jpg');
+(1, 4, '2023-12-29', '80000', 3, 'Ini-Dia-Bukti-Transfer-Mandiri-Dari-ATM-mBanking-dan-Internet-Banking-Mandiri-1.jpg'),
+(2, 4, '2023-12-30', '80000', 3, 'Ini-Dia-Bukti-Transfer-Mandiri-Dari-ATM-mBanking-dan-Internet-Banking-Mandiri-11.jpg'),
+(3, 4, '2024-01-07', '74000', 3, 'Ini-Dia-Bukti-Transfer-Mandiri-Dari-ATM-mBanking-dan-Internet-Banking-Mandiri-12.jpg'),
+(4, 4, '2024-01-07', '80000', 3, 'Ini-Dia-Bukti-Transfer-Mandiri-Dari-ATM-mBanking-dan-Internet-Banking-Mandiri-13.jpg');
 
 -- --------------------------------------------------------
 
@@ -211,13 +223,13 @@ ALTER TABLE `barang`
 -- AUTO_INCREMENT for table `barang_keluar`
 --
 ALTER TABLE `barang_keluar`
-  MODIFY `id_bar_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_bar_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `barang_masuk`
 --
 ALTER TABLE `barang_masuk`
-  MODIFY `id_bar_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_bar_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `kategori`
@@ -229,7 +241,7 @@ ALTER TABLE `kategori`
 -- AUTO_INCREMENT for table `pengajuan`
 --
 ALTER TABLE `pengajuan`
-  MODIFY `id_pengajuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pengajuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
